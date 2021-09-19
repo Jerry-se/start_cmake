a6bc45a0177d48d1d457777126dccf0a7a74b382
Jerry <1364812552@qq.com>
2021-09-19 22:17:56 +0800

    resolve compile error in windows

--- README.md
+++ README.md
@@ -6,11 +6,13 @@
     > md build
     > cd build
     > cmake .. -G "Visual Studio 14 2015"
-    若要指定构建目录D:\code\Git\start_cmake，请使用以下命令
+    # 若要指定构建目录D:\code\Git\start_cmake，请使用以下命令
     > cmake . -G "Visual Studio 14 2015" -Bd:/code/Git/start_cmake/build
+    # 编译
+    > cmake --build . --config Release
 ```
 
-# Linux使用脚本编译
+# Linux使用脚本构建和编译
 ```
     $ ./build.sh
 ```
--- src/dynamic_lib/public.h
+++ src/dynamic_lib/public.h
@@ -2,14 +2,14 @@
 #define TEST_DYNAMIC_LIB_PUBLIC_H_
 
 #if defined(_WIN32)
-#if defined(MYSDK_EXPORTS)
+#if defined(MYSDK_IMPLEMENTATION)
 // On Windows system, functions are exported in a DLL
 #define MY_EXPORT __declspec(dllexport)
 #else
 #define MY_EXPORT __declspec(dllimport)
 #endif
 #else
-#if defined(MYSDK_EXPORTS)
+#if defined(MYSDK_IMPLEMENTATION)
 #define MY_EXPORT __attribute__((visibility("default")))
 #else
 #define MY_EXPORT
